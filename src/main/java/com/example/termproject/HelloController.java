a1a7ca4151264bfb2424595860adf66b8f8cf80d
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-04 14:16:06 -0700

    Update Circle to randomly appear on click

--- src/main/java/com/example/termproject/HelloController.java
+++ src/main/java/com/example/termproject/HelloController.java
@@ -1,14 +1,34 @@
 package com.example.termproject;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
+    private final int MAX_LENGTH = 800;
+    private final int MAX_WIDTH = 600;
     @FXML
     private Label welcomeText;
+    @FXML
+    private Circle bullsEye;
+    @FXML
+    private Pane pane;
+    @FXML
+    private Button button;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        button.setVisible(false);
+        bullsEye.setVisible(true);
+    }
+    @FXML
+    protected void changeLocation(){
+        bullsEye.setFill(Color.BLACK);
+        bullsEye.setCenterX(Math.random()*MAX_LENGTH);
+        bullsEye.setCenterY(Math.random()*MAX_WIDTH);
     }
 }
