25f25f32e076c5ec1c28e5bb4db3979c5a1a9703
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-04 14:50:49 -0700

    Clean up file

--- src/main/java/com/example/termproject/GameController.java
+++ src/main/java/com/example/termproject/GameController.java
@@ -1,17 +1,20 @@
 package com.example.termproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 public class GameController {
-    private final int MAX_LENGTH = 800;
-    private final int MAX_WIDTH = 600;
+    private final double MAX_LENGTH = 600;
+    private final double MAX_WIDTH = 800;
+
     @FXML
-    private Label welcomeText;
+    private BorderPane gameWindow;
     @FXML
     private Circle bullsEye;
     @FXML
