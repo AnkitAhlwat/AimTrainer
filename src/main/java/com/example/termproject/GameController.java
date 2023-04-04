cf9c83c697471d7a8120fd82c9e6a0663477bf86
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-04 15:37:35 -0700

    Debug file to handle edge cases of Window Pane

--- src/main/java/com/example/termproject/GameController.java
+++ src/main/java/com/example/termproject/GameController.java
@@ -1,18 +1,19 @@
 package com.example.termproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
+
+import java.util.Random;
 
 public class GameController {
-    private final double MAX_LENGTH = 600;
+    private final double MAX_HEIGHT = 600;;
     private final double MAX_WIDTH = 800;
 
+    private final Random randomCircleGenerator = new Random();
     @FXML
     private BorderPane gameWindow;
     @FXML
@@ -28,9 +29,25 @@ public class GameController {
         bullsEye.setVisible(true);
     }
     @FXML
-    protected void changeLocation(){
+    private void changeLocation(){
         bullsEye.setFill(Color.BLACK);
-        bullsEye.setCenterX(Math.random()*MAX_LENGTH);
-        bullsEye.setCenterY(Math.random()*MAX_WIDTH);
+        System.out.println(getXLowerBound());
+        System.out.println(getXUpperBound());
+        System.out.println(getXLowerBound());
+        System.out.println(getYUpperBound());
+        bullsEye.setCenterX(randomCircleGenerator.nextDouble(getXLowerBound(),getXUpperBound()));
+        bullsEye.setCenterY(randomCircleGenerator.nextDouble(getYLowerBound(),getYUpperBound()));
+    }
+    private double getYUpperBound(){
+        return MAX_HEIGHT-bullsEye.getRadius();
+    }
+    private double getYLowerBound(){
+        return bullsEye.getRadius();
+    }
+    private double getXUpperBound(){
+        return MAX_WIDTH -bullsEye.getRadius();
+    }
+    private double getXLowerBound(){
+        return bullsEye.getRadius();
     }
 }
