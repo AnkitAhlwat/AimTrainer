ba30fc3501b40245f39f209cd1adf8d23b1fe8cd
Jas Randhawa <60027350+jasr1@users.noreply.github.com>
2023-04-04 21:29:25 -0700

    Add code to change border when selected

--- src/main/java/com/example/termproject/ChangeMap.java
+++ src/main/java/com/example/termproject/ChangeMap.java
@@ -1,10 +1,14 @@
 package com.example.termproject;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 import static com.example.termproject.GameApplication.stage;
@@ -15,6 +19,10 @@ public class ChangeMap {
     private Button backButton;
 
     @FXML
+    private BorderPane nuketown;
+
+
+    @FXML
     void handleBackButton(ActionEvent event) throws IOException {
         // TODO: Handle back button click
         FXMLLoader fxmlLoadermm = new FXMLLoader(GameApplication.class.getResource("MainMenu.fxml"));
@@ -22,6 +30,11 @@ public class ChangeMap {
         stage.setScene(mainmenuscene);
         stage.show();
     }
+    @FXML
+    public void setBorder(){
+        nuketown.setStyle("-fx-border-color: red; -fx-border-width: 2");
+    }
+
 
 }
 
