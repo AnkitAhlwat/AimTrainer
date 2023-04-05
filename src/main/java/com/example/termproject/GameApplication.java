2a43ee2e67a4cd6e686058b635470ea08819f8a8
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-05 16:19:46 -0700

    Add main menu audio

--- src/main/java/com/example/termproject/GameApplication.java
+++ src/main/java/com/example/termproject/GameApplication.java
@@ -2,11 +2,16 @@ package com.example.termproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.IOException;
 
 public class GameApplication extends Application {
 
+    private MediaPlayer mediaPlayer;
     public static Stage stage = null;
     public static Scene scene = null;
 
@@ -22,8 +27,14 @@ public class GameApplication extends Application {
         Scene mainmenuscene = new Scene(fxmlLoadermm.load(), 320, 600);
         stage.setScene(mainmenuscene);
         stage.show();
+        playMusic();
     }
-
+    private void playMusic() {
+        String musicFile = "src/main/resources/mainMenu.mp3";
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+    }
     public static void main(String[] args) {
         System.setProperty("javafx.preloader", LauncherPreloader.class.getName());
         launch(args);
