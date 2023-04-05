87bcb7f6f61d0cc3f1b8384f0df63bf474337683
Jas Randhawa <60027350+jasr1@users.noreply.github.com>
2023-04-05 12:36:45 -0700

    Add code to change border to white when selected and deselect any other selected map

--- src/main/java/com/example/termproject/ChangeMap.java
+++ src/main/java/com/example/termproject/ChangeMap.java
@@ -4,10 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
@@ -20,11 +23,22 @@ public class ChangeMap {
 
     @FXML
     private BorderPane nuketown;
+    
+    @FXML
+    private BorderPane terminal;
+    
+    @FXML
+    private BorderPane spongebobhouse;
+    
+    @FXML
+    private BorderPane afghan;
+
+    @FXML
+    private AnchorPane anchorPane;
 
 
     @FXML
     void handleBackButton(ActionEvent event) throws IOException {
-        // TODO: Handle back button click
         FXMLLoader fxmlLoadermm = new FXMLLoader(GameApplication.class.getResource("MainMenu.fxml"));
         Scene mainmenuscene = new Scene(fxmlLoadermm.load(), 320, 600);
         stage.setScene(mainmenuscene);
@@ -32,7 +46,24 @@ public class ChangeMap {
     }
     @FXML
     public void setBorder(){
-        nuketown.setStyle("-fx-border-color: red; -fx-border-width: 2");
+        nuketown.setStyle("-fx-border-color: white; -fx-border-width: 2");
+    }
+
+    @FXML
+    public void clearBorder(){
+        for(Node node : anchorPane.getChildren()){
+            if(node instanceof BorderPane) {
+                node.setStyle("-fx-border-width: 0");
+            }
+        }
+    }
+
+    @FXML
+    private void handleBorderPaneClicked(MouseEvent event) {
+        BorderPane clickedBorderPane = (BorderPane) event.getSource();
+        String borderPaneId = clickedBorderPane.getId();
+        clearBorder();
+        clickedBorderPane.setStyle("-fx-border-color: white; -fx-border-width: 2");
     }
 
 
